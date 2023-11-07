3b5edc5fbfe8b2f693d1550eac06dd1c57964a23
Crowdbotics <team@crowdbotics.com>
2023-11-07 22:58:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fifteen-fashions-44405",
+  url: "https://fifteen-fashions-44405.botics.co",
+  api: "https://fifteen-fashions-44405.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
